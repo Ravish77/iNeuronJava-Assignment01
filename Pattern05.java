63623fd7e464f398c72e4c55bd1eda15934f3f74
Ravish Rama Bhat <77438775+Ravish77@users.noreply.github.com>
2022-07-27 21:19:33 +0530

    Add files via upload

---
+++ Pattern05.java
@@ -0,0 +1,19 @@
+
+public class Pattern05 {
+	public static void main(String[] args) {
+		int n=11;
+		
+		for(int i=0;i<=n-1;i++) {
+			for(int j=0;j<=n-1;j++) {
+				if((i+j<=(n-1)/2)|| (i-j>=(n-1)/2) || i==0 || i==n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
